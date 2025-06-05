a53d04bf74390626aca52c50edfe5ce322f1bd41
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2025-06-05 18:37:13 +0530

    Assignment

--- Proj05-JDBCFetchData/src/main/java/com/nit/DAO/EmployeeDAOImpl.java
+++ Proj05-JDBCFetchData/src/main/java/com/nit/DAO/EmployeeDAOImpl.java
@@ -3,16 +3,20 @@ package com.nit.DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.nit.Beans.Employee;
 
+@Repository("empDAO")
 public class EmployeeDAOImpl implements IEmployeeDAO {
-	public static final String GET_ALL_EMPLOYEES_BY_DESGS = "SELECT * FROM EMP WHERE EDESG IN(?,?,?)";
+	public static final String GET_ALL_EMPLOYEES_BY_DESGS = "SELECT * FROM EMP11 WHERE EDESG IN(?,?,?)";
 	
 	    @Autowired
 	    private DataSource ds;
@@ -22,7 +26,7 @@ public class EmployeeDAOImpl implements IEmployeeDAO {
 	public List<Employee> getAllEmployeesByDesgs(String desg1, String desg2, String desg3) throws Exception {
 		
 		
-		List<Employee> list = null;
+		List<Employee> list = new ArrayList<Employee>();
 		try (Connection con=ds.getConnection();PreparedStatement ps=con.prepareStatement(GET_ALL_EMPLOYEES_BY_DESGS);) {
 			ps.setString(1, desg1);
 			ps.setString(2, desg2);
@@ -31,8 +35,15 @@ public class EmployeeDAOImpl implements IEmployeeDAO {
 			try(ResultSet rs = ps.executeQuery()){
 				while(rs.next()) {
 					Employee emp = new Employee();
+					emp.setEid(rs.getString(1));
+					emp.setEname(rs.getString(2));
+					emp.setEdesg(rs.getString(3));
+					emp.setBsal(rs.getFloat(4));
+					emp.setHra(rs.getFloat(5));
+					emp.setDa(rs.getFloat(6));
+					emp.setTotsal(rs.getFloat(7));
 			
-					
+					list.add(emp);
 					
 					
 				}
@@ -40,14 +51,19 @@ public class EmployeeDAOImpl implements IEmployeeDAO {
 			
 			
 			
-		return list;
+	
 			
+		}
+		catch (SQLException f) {
+			f.printStackTrace();
+			throw f;
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		}
-		//return null;
+		return list;
+		
 	}
 
 }
